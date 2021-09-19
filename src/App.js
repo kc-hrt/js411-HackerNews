43d097ccd7f7fcff7e808632945495f249961989
kc-hrt <kaseyh@pm.me>
2021-09-19 15:53:02 -0500

    working start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Article from "./Components/Article";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const URL = 'http://hn.algolia.com/api/v1/search';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+    };
+  }
+
+  async componentDidMount() {
+    console.log('mounted 🌵', this.state.articles);
+    const res = await fetch(URL);
+    const data = await res.json();
+    this.setState({
+      articles: data.hits,
+    });
+  }
+
+  componentDidUpdate() {
+    console.log('updated ☀️', this.state.articles);
+  }
+
+  render() {
+    return <div className='grid'>
+      {this.state.articles.map((article) => {
+        return <Article singleArticle={article} />
+      })}
+    </div>;
+  }
 }
 
 export default App;
---
+++ src/Components/Article.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import "../App.css";
+
+export default class Article extends Component {
+  // state = {liked: false};
+  
+  // clicked = () => {
+  //   this.setState({ liked: !this.state.liked });
+  //   console.log('🌈', this.state.liked);
+  // };
+
+  constructor(props) {
+    super(props);
+
+    console.log('🦖',props);
+    console.log('🦕',props.singleArticle.created_at);
+        
+  }
+
+  render() {
+    // let btnColor = this.state.liked ? "green" : "black";
+    // let btnText = this.state.liked ? "👍" : "like";
+    return (
+      <article>
+        <a className="title" href={this.props.singleArticle.url}>{this.props.singleArticle.title}</a>
+        <span className="url-text">{this.props.singleArticle.url}</span>
+        <div className="story-text">
+          {this.props.singleArticle.points} | {this.props.singleArticle.author} | {this.props.singleArticle.created_at} | {this.props.singleArticle.num_comments} comments
+        </div>
+      </article>
+    )
+  }
+}
