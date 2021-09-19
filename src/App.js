fb21affafa931728996656bd63c18fcb51ac3131
unknown <slisette127@gmail.com>
2021-09-19 14:04:03 -0500

    fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const URL = "https://hn.algolia.com/api/v1/search?tags=front_page";
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      arrayOfNews: [], 
+    }
+  }
+
+
+  componentDidMount(){
+    console.log(this.state.arrayOfNews)
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => 
+        this.setState({
+          arrayOfNews: data
+        }) 
+        );
+      } 
+
+
+   render(){
+    return (
+      <div >
+        hello
+      </div>
+     )
+  };
+};
+
 
 export default App;
